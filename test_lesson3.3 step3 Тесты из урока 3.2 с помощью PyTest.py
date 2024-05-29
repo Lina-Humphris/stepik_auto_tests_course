be964d43f87129a2be727e56f491ccf2ae744fd7
Lina-Humphris <postcrossing2002@mail.ru>
2024-05-29 15:22:36 +0300

    Test from lesson 3.2 with PyTest

---
+++ test_lesson3.3 step3 Тесты из урока 3.2 с помощью PyTest.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import unittest
+
+class TestFullForm(unittest.TestCase):
+    def test_form1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        input1 = browser.find_element(By.TAG_NAME, "input")
+        input1.send_keys("Meow")
+        input2 = browser.find_element(By.XPATH, "//div[@class='first_block']/div[@class='form-group second_class']/input")
+        input2.send_keys("Meow")
+        input3 = browser.find_element(By.CLASS_NAME, "form-control.third")
+        input3.send_keys("meow@mail.ru")
+
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Texts in tests are not equal")
+        
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(5)
+        # закрываем браузер после всех манипуляций
+        browser.quit() 
+    def test_form2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        input1 = browser.find_element(By.TAG_NAME, "input")
+        input1.send_keys("Meow")
+        input2 = browser.find_element(By.XPATH, "//div[@class='first_block']/div[@class='form-group second_class']/input")
+        input2.send_keys("Meow")
+        input3 = browser.find_element(By.CLASS_NAME, "form-control.third")
+        input3.send_keys("meow@mail.ru")
+
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Texts in tests are not equal")
+        
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(5)
+        # закрываем браузер после всех манипуляций
+        browser.quit() 
+
+if __name__ == "__main__":
+    unittest.main()
